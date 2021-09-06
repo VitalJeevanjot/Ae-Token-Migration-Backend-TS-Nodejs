d9eb05f0682e2f66c03e2f2196ea5feeb2a26464
Genievot <40867747+genievot@users.noreply.github.com>
2021-09-06 19:46:05 +0530

    adding merkle script tree

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-
+import MerkleTree from './createMerkleTree'
+import * as data from './data/final-token-holders-sorted-upperCase.json'
 const app = express();
 
+var treed = new MerkleTree(data)
+console.log(treed)
+
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Helloi');
 })
 
 app.listen(3000, () => console.log('Running!'))
--- src/controllers/holderController.ts
+++ src/controllers/holderController.ts
@@ -3,9 +3,13 @@ import Holder from './../holders';
 
 
 // Endpoints...
-// GET - / - tree status.
+// GET - / - tree status, Root hash of merkel tree & number of leafs/nodes.
 // GET - /info/<eth_address> - get info from eth address.
 // GET - /hash/<index> - get hash from leaf index.
 // GET - /siblings/<index> - get intermediary hashes (array) by given leaf index.
 // POST - /validate - Check validity of request.
 // POST - /migrate - Migrate tokens.
+
+export var tree = (req: Request, res: Response) => {
+  var result = Holder.
+}
