2f92c7ed50d03a17ecb55b34dbd5a54f03389040
Genievot <40867747+genievot@users.noreply.github.com>
2021-09-09 19:45:43 +0530

    controller handler

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,11 @@
-import express from 'express';
-import MerkleTree from './createMerkleTree'
-import data from './data/final-token-holders-sorted.json'
+import express from "express";
+import * as holderController from './controllers/holderController';
+
 const app = express();
+var port = 3000;
 
 
-var data_to_array: string[] = []
+app.get('/', holderController.rootHash);
 
-data_to_array = Object.entries(data).map((e) => ( JSON.stringify({ [e[0]]: e[1] }).replace(/{|}|"/g, '').toUpperCase()));
-
-
-// console.log("data_to_array--")
-// console.log(data_to_array)
-console.log("data_to_array length--")
-console.log(data_to_array.length)
-var tree = new MerkleTree(data_to_array)
-
-var values = Object.values(data);
-var sumValues = 0n
-// for (let index = 0; index < values.length; index++) {
-//   // console.log(index)
-//   sumValues = BigInt(values[index]) + (sumValues+'n');
-// }
-// console.log("sumValues--")
-// console.log(sumValues)
-// console.log("Get Tree--")
-// console.log(tree.getTree())
-console.log("Get Root Hash--")
-console.log(tree.getRootHash())
-// console.log("Get Index--")
-// console.log(tree.getIndexFromHash('310E3A573B9299000F054D25F0D301CD314C081E4C4E03E8CC07B660B8B4CC95'))
-
-app.get('/', (req, res) => {
-  res.send(data_to_array);
-})
-
-app.listen(3000, () => console.log('Running!'))
+app.listen(port, () => console.log(`Running at ${port}!`))
 
--- src/controllers/holderController.ts
+++ src/controllers/holderController.ts
@@ -1,6 +1,13 @@
-import { Request, Repsonse } from 'express';
+import { Request, Response } from 'express';
 import Holder from './../holders';
 
+import MerkleTree from '../createMerkleTree'
+import data from '../data/final-token-holders-sorted.json'
+
+
+var data_to_array: string[] = []
+data_to_array = Object.entries(data).map((e) => ( JSON.stringify({ [e[0]]: e[1] }).replace(/{|}|"/g, '').toUpperCase()));
+var tree = new MerkleTree(data_to_array)
 
 // Endpoints...
 // GET - / - tree status, Root hash of merkel tree & number of leafs/nodes.
@@ -10,6 +17,35 @@ import Holder from './../holders';
 // POST - /validate - Check validity of request.
 // POST - /migrate - Migrate tokens.
 
-export var tree = (req: Request, res: Response) => {
-  var result = Holder.
+export let rootHash = (req: Request, res: Response) => {
+  return res.send({"status": true, "tree": {"root": tree.getRootHash(), "length": tree.leaves.length}});
 }
+
+
+
+
+
+// logs
+
+
+
+
+// console.log("data_to_array--")
+// console.log(data_to_array)
+// console.log("data_to_array length--")
+
+
+// var values = Object.values(data);
+// var sumValues = 0
+// for (let index = 0; index < values.length; index++) {
+//   // console.log(index)
+//   sumValues = BigInt(values[index]) + (sumValues+'n');
+// }
+// console.log("sumValues--")
+// console.log(sumValues)
+// console.log("Get Tree--")
+// console.log(tree.getTree())
+// console.log("Get Root Hash--")
+// console.log(tree.getRootHash())
+// console.log("Get Index--")
+// console.log(tree.getIndexFromHash('310E3A573B9299000F054D25F0D301CD314C081E4C4E03E8CC07B660B8B4CC95'))
